c737508801923bc422a26c430d3ce232b558de0f
annmaryvinod <annmarypriyavinod@gmail.com>
2022-02-28 23:26:21 +0530

    created Home component,imported and nested it in the App component

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
 
@@ -8,7 +9,7 @@ function App() {
     <div className="App">
       <Navbar/>
       <div className="content">
-        <h1> App Component </h1>
+       <Home/>
         
       </div>
     </div>
