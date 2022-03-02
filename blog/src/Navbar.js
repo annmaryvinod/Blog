84c1c978b514632d82ccc72efe79eef2099432c7
annmaryvinod <annmarypriyavinod@gmail.com>
2022-03-02 21:55:55 +0530

    deleted inline-styles

--- blog/src/Navbar.js
+++ blog/src/Navbar.js
@@ -4,12 +4,7 @@ const Navbar = () => {
             <h1> The Dojo Blog</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create" style = {{
-                    color : "white",
-                    backgroundColor : "#f1356d",
-                    // Note : the camelcase
-                    borderRadius : "8px"
-                }}>New Blog</a>
+                <a href="/create">New Blog</a>
 
             </div>
         </nav>
