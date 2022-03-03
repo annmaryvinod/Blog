fa35615c746d0feeffb3deebaad341ce0d7bc837
annmaryvinod <annmarypriyavinod@gmail.com>
2022-03-03 23:16:45 +0530

    created handleClick function

--- blog/src/Home.js
+++ blog/src/Home.js
@@ -1,7 +1,12 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log("Hello,ninjas!");
+    }
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button>Click me</button>
         </div>
      );
 }
