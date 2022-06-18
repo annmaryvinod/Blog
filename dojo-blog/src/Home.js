0bb7e315bca2545bfe6a0963977fce819ba193eb
annmaryvinod <annmarypriyavinod@gmail.com>
2022-06-18 07:48:30 -0400

    how to handle a function

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+
+    const handleClick = () =>{
+        console.log("hahahaha");
+    }
+
+    const handle =() =>{
+        console.log("Already invoked!");
+    }
     return ( 
         <div className="home">
             <h2>Home Page</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={handle()}>Click</button>
         </div>
      );
 }
