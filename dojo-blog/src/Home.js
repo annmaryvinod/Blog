df05027722dc0fdb530159c55ddbb2b185838f1e
annmaryvinod <annmarypriyavinod@gmail.com>
2022-06-18 08:08:11 -0400

    handling functions with parameters

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -7,11 +7,17 @@ const Home = () => {
     const handle =() =>{
         console.log("Already invoked!");
     }
+
+    const hello =(name)=>{
+        console.log('hello ' + name);
+    }
     return ( 
         <div className="home">
             <h2>Home Page</h2>
             <button onClick={handleClick}>Click me</button>
             <button onClick={handle()}>Click</button>
+            <br />
+            <button onClick = {()=> {hello('hari')}}>Click This</button>
         </div>
      );
 }
